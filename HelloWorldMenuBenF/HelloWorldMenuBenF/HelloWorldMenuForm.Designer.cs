4249e697176b4aab38619c048a4b1b895e7b20c4
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-09-07 11:05:00 -0400

    initial Commit

---
+++ HelloWorldMenuBenF/HelloWorldMenuBenF/HelloWorldMenuForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace HelloWorldMenuBenF
+{
+    partial class frmHelloWorldMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuProgram = new System.Windows.Forms.MenuStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.mnuProgram.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuProgram
+            // 
+            this.mnuProgram.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.mnuProgram.Location = new System.Drawing.Point(0, 0);
+            this.mnuProgram.Name = "mnuProgram";
+            this.mnuProgram.Size = new System.Drawing.Size(1470, 27);
+            this.mnuProgram.TabIndex = 0;
+            this.mnuProgram.Text = "menuStrip1";
+            this.mnuProgram.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.mnuProgram_ItemClicked);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // frmHelloWorldMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1470, 633);
+            this.Controls.Add(this.mnuProgram);
+            this.MainMenuStrip = this.mnuProgram;
+            this.Name = "frmHelloWorldMenu";
+            this.Text = "Hello World With Menu by Ben";
+            this.mnuProgram.ResumeLayout(false);
+            this.mnuProgram.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuProgram;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
+
